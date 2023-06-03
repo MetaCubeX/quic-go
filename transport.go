7b85b8c8f2dfb46e870a5746645d82e8643593fa
wwqgtxx <wwqgtxx@gmail.com>
2023-06-03 15:26:21 +0800

    expose `createdConn` and `isSingleUse` for Transport

--- transport.go
+++ transport.go
@@ -537,3 +537,11 @@ func setTLSConfigServerName(tlsConf *tls.Config, addr net.Addr, host string) {
 	}
 	tlsConf.ServerName = h
 }
+
+func (t *Transport) SetCreatedConn(createdConn bool) {
+	t.createdConn = createdConn
+}
+
+func (t *Transport) SetSingleUse(isSingleUse bool) {
+	t.isSingleUse = isSingleUse
+}
